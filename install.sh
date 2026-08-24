22441bdbe3d7bc8888cd960475bd1c709e8e36a1
Trerrtt <thundermc30@gmail.com>
2026-08-25 02:47:34 +0500

    Fix alembic working directory

--- install.sh
+++ install.sh
@@ -486,7 +486,7 @@ start_backend_dev() {
     
     # Run migrations
     print_info "Running database migrations..."
-    alembic -c "$PROJECT_DIR/migrations/alembic.ini" upgrade head
+    (cd "$PROJECT_DIR" && alembic -c "$PROJECT_DIR/migrations/alembic.ini" upgrade head)
     
     print_success "Backend ready! Starting server with hot reload..."
     print_info "API will be available at: http://localhost:8000"
